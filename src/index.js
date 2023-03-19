f13221f8b0f2101a42a43f29c4c08a18294d7090
Saurabh Ashok Mhaske <78920928+mhaskesaurabh3@users.noreply.github.com>
2023-03-19 19:55:22 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import Store from "./Shared/Store";
 
@@ -17,7 +16,5 @@ root.render(
   </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
